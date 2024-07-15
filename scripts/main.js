ebf8781dcc4c4f747f838ad02a6fabefa92205d6
GaonaAyelen <gaonaayelen2@gmail.com>
2024-07-15 17:19:48 -0300

    ultimos detalles de css

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,39 @@
- function bienvenido(){
+
+
+function bienvenido() {
     let nombre = prompt("¿Como te llamas?")
 
-    alert("Hola "+ nombre + " ¿Como estas?")
- }
-  bienvenido()
+    alert("Hola " + nombre + " ¿Como estas?")
+}
+bienvenido()
 
 
 
-const servicios  = ["1 - Soldadura" , "2 - Fabricacion", "3 - Reparaciones" , "4 - Montajes industriales"]
+const servicios = ["1 - Soldadura", "2 - Fabricacion", "3 - Reparaciones", "4 - Montajes industriales"]
 
 const Precios = [100, 200, 300, 400]
 
 
 
-function elegirServicio () {
+function elegirServicio() {
 
-    alert("Bienvenido a nuestra tienda, donde encontraras estos servicios..\n"  +
-          "1. Soldadura\n" +
-          "2. Fabricacion\n" +
-          "3. Reparaciones\n" +
-          "4. Montajes industriales");
+    alert("Bienvenido a nuestra tienda, donde encontraras estos servicios..\n" +
+        "1. Soldadura\n" +
+        "2. Fabricacion\n" +
+        "3. Reparaciones\n" +
+        "4. Montajes industriales");
 
-    /* Solicitar al usuario que ingrese su eleccion*/
+
     let opcion = prompt("Ingresa el número correspondiente al servicio que desea solicitar:\n" +
         "1. Soldadura\n" +
         "2. Fabricacion\n" +
         "3. Reparaciones\n" +
         "4. Montajes industriales");
 
-    
+
     opcion = parseInt(opcion);
 
-     /*Validar la opcion ingresada por el usuario*/
+
     switch (opcion) {
         case 1:
             confirm("Has solicitado el servicio de soldadura.");
@@ -47,13 +49,36 @@ function elegirServicio () {
             break;
         default:
             confirm("Opción no válida. Por favor, selecciona un número del 1 al 4.");
-            
+
             elegirServicio();
             break;
     }
 }
-
 elegirServicio();
+
+
+let usuarioDuenio = confirm("¿Sos el dueño de esta pagina?");
+
+if (usuarioDuenio) {
+
+    let agregueServicio = prompt("Puede agregar el nuevo servicio..");
+
+    servicios.push(agregueServicio);
+
+    let servicioAgregado = alert("Se agrego el nuevo servicio.. Gracias!");
+
+    servicioAgregado()
+
+
+} else {
+    let inicio = confirm("Gracias por visitar nuestra pagina.. por favor lo invitamos a elegir el servicio por el cual esta interesado!")
+
+    let seleccion = parseInt(inicio);
+
+
+    elegirServicio(parseInt(seleccion));
+
+}
 
 
 
